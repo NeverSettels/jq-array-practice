a8772ab49701a17033ee73561633306aaf3bfdb1
Eric Settels and Patrick Delaney <eric+patrick@>
2020-01-21 10:03:43 -0800

    swiched it nain js ti use forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
   $('#formOne').submit(function(event){
-    var dataArr = [$('#thing1').val(),$('#thing2').val(),$('#thing3').val()];
+    // var dataArr = [$('#thing1').val(),$('#thing2').val(),$('#thing3').val()];
 
-    $('#thing1listI').text(dataArr[0]);
-    $('#thing2listI').text(dataArr[1]);
-    $('#thing3listI').text(dataArr[2]);
+    // $('#thing1listI').text(dataArr[0]);
+    // $('#thing2listI').text(dataArr[1]);
+    // $('#thing3listI').text(dataArr[2]);
+
+    var blanks = ['thing1','thing2','thing3'] 
+    blanks.forEach(function(blank){
+      
+        var input = $("#" + blank).val();
+        $("#" + blank + "listI").text(input);
+
+    })
 
     event.preventDefault();
   });
